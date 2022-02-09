00dafc9f353ef9817402c29a65ef5415ea3dea1a
MonsterPrograms <34608175+MonsterPrograms@users.noreply.github.com>
2022-02-09 14:14:18 +0000

    Initial release

---
+++ PeekBaccaratCalculator/PeekBaccaratCalculator/MainForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace PeekBaccaratCalculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPlayerCard1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBankerCard1 = new System.Windows.Forms.TextBox();
+            this.txtPlayerCard2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBankerCard2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.chkTopMost = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPlayerOrBanker = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPlayerCard1
+            // 
+            this.txtPlayerCard1.Location = new System.Drawing.Point(71, 38);
+            this.txtPlayerCard1.Name = "txtPlayerCard1";
+            this.txtPlayerCard1.Size = new System.Drawing.Size(88, 20);
+            this.txtPlayerCard1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "1st Card:";
+            // 
+            // txtBankerCard1
+            // 
+            this.txtBankerCard1.Location = new System.Drawing.Point(71, 90);
+            this.txtBankerCard1.Name = "txtBankerCard1";
+            this.txtBankerCard1.Size = new System.Drawing.Size(88, 20);
+            this.txtBankerCard1.TabIndex = 3;
+            // 
+            // txtPlayerCard2
+            // 
+            this.txtPlayerCard2.Location = new System.Drawing.Point(71, 64);
+            this.txtPlayerCard2.Name = "txtPlayerCard2";
+            this.txtPlayerCard2.Size = new System.Drawing.Size(88, 20);
+            this.txtPlayerCard2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "2nd Card:";
+            // 
+            // txtBankerCard2
+            // 
+            this.txtBankerCard2.Location = new System.Drawing.Point(71, 116);
+            this.txtBankerCard2.Name = "txtBankerCard2";
+            this.txtBankerCard2.Size = new System.Drawing.Size(88, 20);
+            this.txtBankerCard2.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "4th Card:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 93);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "3rd Card:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(71, 171);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(88, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // chkTopMost
+            // 
+            this.chkTopMost.AutoSize = true;
+            this.chkTopMost.Location = new System.Drawing.Point(71, 200);
+            this.chkTopMost.Name = "chkTopMost";
+            this.chkTopMost.Size = new System.Drawing.Size(68, 17);
+            this.chkTopMost.TabIndex = 8;
+            this.chkTopMost.Text = "TopMost";
+            this.chkTopMost.UseVisualStyleBackColor = true;
+            this.chkTopMost.CheckedChanged += new System.EventHandler(this.chkTopMost_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "P/B:";
+            // 
+            // txtPlayerOrBanker
+            // 
+            this.txtPlayerOrBanker.Location = new System.Drawing.Point(71, 12);
+            this.txtPlayerOrBanker.Name = "txtPlayerOrBanker";
+            this.txtPlayerOrBanker.Size = new System.Drawing.Size(88, 20);
+            this.txtPlayerOrBanker.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Calculate);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(171, 227);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPlayerOrBanker);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chkTopMost);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPlayerCard2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPlayerCard1);
+            this.Controls.Add(this.txtBankerCard2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBankerCard1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Peek Baccarat Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPlayerCard1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBankerCard1;
+        private System.Windows.Forms.TextBox txtPlayerCard2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBankerCard2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.CheckBox chkTopMost;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPlayerOrBanker;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PeekBaccaratCalculator/PeekBaccaratCalculator/MainForm.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PeekBaccaratCalculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate(object sender, EventArgs e)
+        {
+            // initialize all the variables we need before the loop starts
+            var random = new Random();
+            int index;
+            int playerTotal;
+            int bankerTotal;
+            string stringPlayerTotal;
+            string stringBankerTotal;
+            int playerCard3 = 0;
+            int bankerCard3 = 0;
+            int playerWins = 0;
+            int bankerWins = 0;
+            int ties = 0;
+
+            // 20,000 simulations begin
+            for (var i = 0; i < 20000; i++)
+            {
+                // a fresh 8 deck shoe for beggining of each game round
+                var shoe = new List<int> { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1 };
+
+                // find out what cards have already been dealt and remove those from the shoe to get the most accurate shoe as possible
+                if (int.TryParse(txtPlayerCard1.Text, out int playerCard1) && playerCard1 >= 1 && playerCard1 <= 10)
+                {
+                    index = shoe.IndexOf(playerCard1);
+                    if (index > -1)
+                        shoe.RemoveAt(index);
+                }
+                if (int.TryParse(txtPlayerCard2.Text, out int playerCard2) && playerCard2 >= 1 && playerCard2 <= 10)
+                {
+                    index = shoe.IndexOf(playerCard2);
+                    if (index > -1)
+                        shoe.RemoveAt(index);
+                }
+                if (int.TryParse(txtBankerCard1.Text, out int bankerCard1) && bankerCard1 >= 1 && bankerCard1 <= 10)
+                {
+                    index = shoe.IndexOf(bankerCard1);
+                    if (index > -1)
+                        shoe.RemoveAt(index);
+                }
+                if (int.TryParse(txtBankerCard2.Text, out int bankerCard2) && bankerCard2 >= 1 && bankerCard2 <= 10)
+                {
+                    index = shoe.IndexOf(bankerCard2);
+                    if (index > -1)
+                        shoe.RemoveAt(index);
+                }
+
+                // both player and banker will receive 2 cards each, so let's get random cards/values for the ones that have not been peeked yet
+                if (playerCard1 == 0)
+                {
+                    index = random.Next(0, shoe.Count);
+                    playerCard1 = shoe[index];
+                    shoe.RemoveAt(index);
+                }
+
+                if (playerCard2 == 0)
+                {
+                    index = random.Next(0, shoe.Count);
+                    playerCard2 = shoe[index];
+                    shoe.RemoveAt(index);
+                }
+
+                if (bankerCard1 == 0)
+                {
+                    index = random.Next(0, shoe.Count);
+                    bankerCard1 = shoe[index];
+                    shoe.RemoveAt(index);
+                }
+
+                if (bankerCard2 == 0)
+                {
+                    index = random.Next(0, shoe.Count);
+                    bankerCard2 = shoe[index];
+                    shoe.RemoveAt(index);
+                }
+
+                // calculate the initial two-card totals for player and banker
+                playerTotal = playerCard1 + playerCard2;
+                bankerTotal = bankerCard1 + bankerCard2;
+
+                // make sure the totals are added correctly by removing the first digit if the player and/or banker total is over 9
+                if (playerTotal > 9)
+                {
+                    stringPlayerTotal = playerTotal.ToString().Remove(0, 1);
+                    playerTotal = int.Parse(stringPlayerTotal);
+                }
+
+                if (bankerTotal > 9)
+                {
+                    stringBankerTotal = bankerTotal.ToString().Remove(0, 1);
+                    bankerTotal = int.Parse(stringBankerTotal);
+                }
+
+                // determine if player needs a third card
+                if (playerTotal >= 0 && playerTotal <= 5)
+                {
+                    index = random.Next(0, shoe.Count);
+                    playerCard3 = shoe[index];
+                    shoe.RemoveAt(index);
+                }
+
+                // determine if banker needs a third card
+                if (bankerTotal >= 0 && bankerTotal <= 2)
+                {
+                    index = random.Next(0, shoe.Count);
+                    bankerCard3 = shoe[index];
+                    shoe.RemoveAt(index);
+                }
+                else if (bankerTotal == 3 && playerCard3 != 8)
+                {
+                    index = random.Next(0, shoe.Count);
+                    bankerCard3 = shoe[index];
+                    shoe.RemoveAt(index);
+                }
+                else if (bankerTotal == 4 && (playerCard3 == 0 || (playerCard3 >= 2 && playerCard3 <= 7)))
+                {
+                    index = random.Next(0, shoe.Count);
+                    bankerCard3 = shoe[index];
+                    shoe.RemoveAt(index);
+                }
+                else if (bankerTotal == 5 && (playerCard3 == 0 || playerCard3 >= 4 && playerCard3 <= 7))
+                {
+                    index = random.Next(0, shoe.Count);
+                    bankerCard3 = shoe[index];
+                    shoe.RemoveAt(index);
+                }
+                else if (bankerTotal == 6 && (playerCard3 == 6 || playerCard3 == 7))
+                {
+                    index = random.Next(0, shoe.Count);
+                    bankerCard3 = shoe[index];
+                    shoe.RemoveAt(index);
+                }
+
+                // add on the third card to the totals for player and banker
+                playerTotal += playerCard3;
+                bankerTotal += bankerCard3;
+
+                // make sure the totals are added correctly by removing the first digit if the player and/or banker total is over 9
+                if (playerTotal > 9)
+                {
+                    stringPlayerTotal = playerTotal.ToString().Remove(0, 1);
+                    playerTotal = int.Parse(stringPlayerTotal);
+                }
+
+                if (bankerTotal > 9)
+                {
+                    stringBankerTotal = bankerTotal.ToString().Remove(0, 1);
+                    bankerTotal = int.Parse(stringBankerTotal);
+                }
+
+                // calculate the end result
+                if (playerTotal > bankerTotal)
+                {
+                    playerWins++;
+                }
+                else if (bankerTotal > playerTotal)
+                {
+                    bankerWins++;
+                }
+                else
+                {
+                    ties++;
+                }
+            }
+
+            // display the optimal strategy at the end of the 20,000 simulations by doing ev calculations
+            if (txtPlayerOrBanker.Text.ToLower() == "player" || txtPlayerOrBanker.Text.ToLower() == "p")
+            {
+                decimal winOdds = (decimal)playerWins / (decimal)(playerWins + bankerWins + ties);
+                decimal loseOdds = (decimal)bankerWins / (decimal)(bankerWins + playerWins + ties);
+                decimal tieOdds = (decimal)ties / (decimal)(playerWins + bankerWins + ties);
+                decimal tripleEv = winOdds * 2.80m - loseOdds * 3.20m - tieOdds * 0.20m;
+                decimal continueEv = winOdds * 0.80m - loseOdds * 1.20m - tieOdds * 0.20m;
+
+                if (tripleEv >= continueEv)
+                {
+                    MessageBox.Show($"Triple\n\nTriple EV: {tripleEv}\nContinue EV: {continueEv}", "Optimal Strategy", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"Continue\n\nTriple EV: {tripleEv}\nContinue EV: {continueEv}", "Optimal Strategy", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else if (txtPlayerOrBanker.Text.ToLower() == "banker" || txtPlayerOrBanker.Text.ToLower() == "b")
+            {
+                decimal winOdds = (decimal)bankerWins / (decimal)(playerWins + bankerWins + ties);
+                decimal loseOdds = (decimal)playerWins / (decimal)(bankerWins + playerWins + ties);
+                decimal tieOdds = (decimal)ties / (decimal)(playerWins + bankerWins + ties);
+                decimal tripleEv = winOdds * 2.65m - loseOdds * 3.20m - tieOdds * 0.20m;
+                decimal continueEv = winOdds * 0.75m - loseOdds * 1.20m - tieOdds * 0.20m;
+
+                if (tripleEv >= continueEv)
+                {
+                    MessageBox.Show($"Triple\n\nTriple EV: {tripleEv}\nContinue EV: {continueEv}", "Optimal Strategy", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"Continue\n\nTriple EV: {tripleEv}\nContinue EV: {continueEv}", "Optimal Strategy", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtPlayerCard1.Clear();
+            txtPlayerCard2.Clear();
+            txtBankerCard1.Clear();
+            txtBankerCard2.Clear();
+        }
+
+        private void chkTopMost_CheckedChanged(object sender, EventArgs e)
+        {
+            TopMost = chkTopMost.Checked;
+        }
+    }
+}
